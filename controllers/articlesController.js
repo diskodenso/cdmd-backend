a51445428ef221296139ed524ce08ed3bda8ad36
Denso666 <Denisolf@mx.de>
2022-05-19 23:56:52 +0200

    add get single article controller

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -7,14 +7,24 @@ import pool from "../db/pg.js";
 // ------ get all articles controller ------//
 export const getAllArticles = (req, res) => {
     pool
-        //SQL SELECT * FROM user - select all data from users table
-        .query("SELECT * FROM aricles")
-        .then((data) => res.status(200).json({ articles: data.rows }))
-        .catch((err) => res.status(500).json(err));
+    //SQL SELECT * FROM user - select all data from users table
+    .query("SELECT * FROM aricles")
+    .then((data) => res.status(200).json({ articles: data.rows }))
+    .catch((err) => res.status(500).json(err));
 };
 
 // ------ get single articles controller ------//
 export const getSingleArticle = (req, res) => {
-    const { id } = req.params;
-    pool.query("SELECT*FROM articles WHERE id=$1", [id])
-}
+  const { id } = req.params;
+  pool
+    .query("SELECT*FROM articles WHERE id=$1", [id])
+    .then((data) => {
+      // if article id is not existing we send error message
+      if (data.rowCount == 0) {
+        res.status(404).send("There is not article matching this id!");
+      } else {
+        res.status(202).json(data.rows[0]);
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+};
